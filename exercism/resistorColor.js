3fa92651ca770752b08e4ca892246693fa69f2d0
Jared Brown <jaredbrown1@hotmail.com>
2021-05-18 17:44:10 -0700

    second pass solution on resistorColor exercism exercise

--- exercism/resistorColor.js
+++ exercism/resistorColor.js
@@ -1,26 +1,41 @@
-export const colorCode = (COLORS) => {
-	switch (COLORS) {
-		case "black":
-			return 0;
-		case "brown":
-			return 1;
-		case "red":
-			return 2;
-		case "orange":
-			return 3;
-		case "yellow":
-			return 4;
-		case "green":
-			return 5;
-		case "blue":
-			return 6;
-		case "violet":
-			return 7;
-		case "grey":
-			return 8;
-		case "white":
-			return 9;
-		default:
-			return undefined;
-	}
+// export const colorCode = (COLORS) => {
+// 	switch (COLORS) {
+// 		case "black":
+// 			return 0;
+// 		case "brown":
+// 			return 1;
+// 		case "red":								<----- first pass
+// 			return 2;
+// 		case "orange":
+// 			return 3;
+// 		case "yellow":
+// 			return 4;
+// 		case "green":
+// 			return 5;
+// 		case "blue":
+// 			return 6;
+// 		case "violet":
+// 			return 7;
+// 		case "grey":
+// 			return 8;
+// 		case "white":
+// 			return 9;
+// 		default:
+// 			return undefined;
+// 	}
+// };
+
+export const colorCode = (color) => {
+	return COLORS.indexOf(color);
 };
+// <----- second pass
+export const COLORS = [
+	"black",
+	"brown",
+	"red",
+	"orange",
+	"yellow",
+	"green",
+	"blue",
+	"violet",
+];
