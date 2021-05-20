2f3b17cc8b8ad89cc1ce4fb043ddd11b82f2490c
Jared Brown <jaredbrown1@hotmail.com>
2021-05-19 20:39:49 -0700

    fcc js challenge

--- fcc/challenges.js
+++ fcc/challenges.js
@@ -30,3 +30,19 @@ function reverseString(str) {
 }
 
 reverseString("hello");
+
+// Return the factorial of the provided integer.
+
+function factorialize(num) {
+	if (num === 0 || num === 1) {
+		return 1;
+	}
+
+	for (var i = num - 1; i >= 1; i--) {
+		num *= i;
+	}
+
+	return num;
+}
+
+factorialize(5);
