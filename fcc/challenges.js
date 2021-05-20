f167bcd1f24dfbf71dcb3f245e505056fab83fd7
Jared Brown <jaredbrown1@hotmail.com>
2021-05-19 20:08:41 -0700

    fcc challenge

--- fcc/challenges.js
+++ fcc/challenges.js
@@ -1,8 +1,20 @@
-// We'll pass you an array of two numbers. Return the sum of those two
-// numbers plus the sum of all the numbers between them. The lowest number will not always come first.
+// // We'll pass you an array of two numbers. Return the sum of those two
+// // numbers plus the sum of all the numbers between them. The lowest number will not always come first.
 
 function sumAll(arr) {
 	return Range(arr[0], arr[1]);
 }
 
 sumAll([1, 4]);
+
+// You are given a variable celsius representing a temperature in Celsius. Use the
+// variable fahrenheit already defined and assign it the Fahrenheit temperature equivalent
+//  to the given Celsius temperature. Use the algorithm mentioned above to help convert the
+//  Celsius temperature to Fahrenheit.
+
+function convertToF(celsius) {
+	let fahrenheit = (celsius * 9) / 5 + 32;
+	return fahrenheit;
+}
+
+convertToF(30);
