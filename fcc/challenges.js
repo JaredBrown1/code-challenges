e4824b8b22a849ee0a1af6295becf24e73591e48
Jared Brown <jaredbrown1@hotmail.com>
2021-05-21 00:23:12 -0700

    fcc js challenge

--- fcc/challenges.js
+++ fcc/challenges.js
@@ -46,3 +46,21 @@ function factorialize(num) {
 }
 
 factorialize(5);
+
+// Return the length of the longest word in the provided sentence.
+
+// Your response should be a number.
+
+function findLongestWordLength(str) {
+	let arr = str.trim().split(" ");
+	let len = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i].length > len) {
+			len = arr[i].length;
+		}
+	}
+	return len;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
