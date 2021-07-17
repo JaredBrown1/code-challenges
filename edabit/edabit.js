a59fb1f90feac0306c2fad78211d652f3bb99fd4
Jared Brown <jaredbrown1@hotmail.com>
2021-07-17 16:52:56 -0700

    edabit js challenge

--- edabit/edabit.js
+++ edabit/edabit.js
@@ -178,3 +178,9 @@ function lessThan100(a, b) {
 	}
 	return false;
 }
+
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+function addition(num) {
+	return num + 1;
+}
