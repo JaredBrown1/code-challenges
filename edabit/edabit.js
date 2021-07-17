7b4da5ed934c1ae5c68a0f339684ff7772c6d948
Jared Brown <jaredbrown1@hotmail.com>
2021-07-17 16:33:17 -0700

    edabit js challenge

--- edabit/edabit.js
+++ edabit/edabit.js
@@ -122,3 +122,9 @@ function nSidedShape(n) {
 function calcAge(age) {
 	return age * 365;
 }
+
+// Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+
+function nextEdge(side1, side2) {
+	return side1 + side2 - 1;
+}
