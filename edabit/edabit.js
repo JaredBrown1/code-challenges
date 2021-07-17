cc293047f9045c25bd0a9518409daae0e839f25a
Jared Brown <jaredbrown1@hotmail.com>
2021-07-17 16:51:56 -0700

    edabit js challenge

--- edabit/edabit.js
+++ edabit/edabit.js
@@ -169,3 +169,12 @@ function points(twoPointers, threePointers) {
 function nameString(name) {
 	return name + "Edabit";
 }
+
+// Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
+
+function lessThan100(a, b) {
+	if (a + b < 100) {
+		return true;
+	}
+	return false;
+}
