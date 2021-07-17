54e7b6c01e3339334c67598e9b5c4f78507f63f0
Jared Brown <jaredbrown1@hotmail.com>
2021-07-17 16:31:43 -0700

    edabit js challenge

--- edabit/edabit.js
+++ edabit/edabit.js
@@ -3,10 +3,10 @@
 //In this challenge, you must verify the equality of two different given parameters: a and b
 
 function checkEquality(a, b) {
-  if (a === b) {
-    return true;
-  }
-  return false;
+	if (a === b) {
+		return true;
+	}
+	return false;
 }
 
 // challenge 2
@@ -14,10 +14,10 @@ function checkEquality(a, b) {
 // Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
 
 function lessThan100(a, b) {
-  if (a + b < 100) {
-    return true;
-  }
-  return false;
+	if (a + b < 100) {
+		return true;
+	}
+	return false;
 }
 
 // challenge 3
@@ -26,10 +26,10 @@ function lessThan100(a, b) {
 // be divided evenly by b. Return false otherwise.
 
 function dividesEvenly(a, b) {
-  if (Number.isInteger(a / b)) {
-    return true;
-  }
-  return false;
+	if (Number.isInteger(a / b)) {
+		return true;
+	}
+	return false;
 }
 
 // challenge 4
@@ -38,9 +38,9 @@ function dividesEvenly(a, b) {
 // array with the elements multiplied by two.
 
 function getMultipliedArr(arr) {
-  return arr.map(i => {
-    return i * 2;
-  });
+	return arr.map((i) => {
+		return i * 2;
+	});
 }
 
 // challenge 5
@@ -61,10 +61,10 @@ let b = arr[1];
 // return "Today, I am feeling neutral".
 
 function moodToday(mood) {
-  if (mood) {
-    return `Today, I am feeling ${mood}`;
-  }
-  return `Today, I am feeling neutral`;
+	if (mood) {
+		return `Today, I am feeling ${mood}`;
+	}
+	return `Today, I am feeling neutral`;
 }
 
 // challenge 7
@@ -73,7 +73,7 @@ function moodToday(mood) {
 // number of frames shown in a given number of minutes for a certain FPS.
 
 function frames(minutes, fps) {
-  return fps * (minutes * 60);
+	return fps * (minutes * 60);
 }
 
 // challenge 8
@@ -81,7 +81,7 @@ function frames(minutes, fps) {
 // Write a function that takes two integers (hours, minutes) and converts them into seconds.
 
 function convert(hours, minutes) {
-  return Math.floor(hours * 60 * 60) + Math.floor(minutes * 60);
+	return Math.floor(hours * 60 * 60) + Math.floor(minutes * 60);
 }
 
 // challenge 9
@@ -91,28 +91,34 @@ function convert(hours, minutes) {
 // Here are the expected outputs to get from these inputs.
 
 function nSidedShape(n) {
-  switch (n) {
-    case 1:
-      return "circle";
-    case 2:
-      return "semi-circle";
-    case 3:
-      return "triangle";
-    case 4:
-      return "square";
-    case 5:
-      return "pentagon";
-    case 6:
-      return "hexagon";
-    case 7:
-      return "heptagon";
-    case 8:
-      return "octagon";
-    case 9:
-      return "nonagon";
-    case 10:
-      return "decagon";
-    default:
-      return null;
-  }
+	switch (n) {
+		case 1:
+			return "circle";
+		case 2:
+			return "semi-circle";
+		case 3:
+			return "triangle";
+		case 4:
+			return "square";
+		case 5:
+			return "pentagon";
+		case 6:
+			return "hexagon";
+		case 7:
+			return "heptagon";
+		case 8:
+			return "octagon";
+		case 9:
+			return "nonagon";
+		case 10:
+			return "decagon";
+		default:
+			return null;
+	}
+}
+
+// Create a function that takes the age in years and returns the age in days.
+
+function calcAge(age) {
+	return age * 365;
 }
