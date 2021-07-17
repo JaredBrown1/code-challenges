a00be704c82bd525b05c5c0087888a60bd5a2dba
Jared Brown <jaredbrown1@hotmail.com>
2021-07-17 16:39:13 -0700

    edabit js challenge

--- edabit/edabit.js
+++ edabit/edabit.js
@@ -140,3 +140,11 @@ function convert(minutes) {
 function findPerimeter(length, width) {
 	return length * 2 + width * 2;
 }
+
+// There is a single operator in JavaScript, capable of providing the remainder
+// of a division operation. Two numbers are passed as parameters. The first parameter
+// divided by the second parameter will have a remainder, possibly zero. Return that value.
+
+function remainder(x, y) {
+	return x % y;
+}
