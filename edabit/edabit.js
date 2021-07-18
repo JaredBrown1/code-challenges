13d71f940db689afc90f5be5301654f2f81a822d
Jared Brown <jaredbrown1@hotmail.com>
2021-07-17 17:03:06 -0700

    edabit js challenge

--- edabit/edabit.js
+++ edabit/edabit.js
@@ -200,3 +200,9 @@ function and(a, b) {
 	}
 	return false;
 }
+
+// Write a function that returns the string "something" joined with a space " " and the given argument a.
+
+function giveMeSomething(a) {
+	return `something ${a}`;
+}
