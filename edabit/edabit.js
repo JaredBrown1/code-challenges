7c335094760c0a9a790f40908d34a0c458d56184
Jared Brown <jaredbrown1@hotmail.com>
2021-11-15 17:22:48 -0800

    edabit challenge

--- edabit/edabit.js
+++ edabit/edabit.js
@@ -273,3 +273,9 @@ function makesTen(a, b) {
 		return false;
 	}
 }
+
+// Given an n-sided regular polygon n, return the total sum of internal angles (in degrees).
+
+function sumPolygon(n) {
+	return (n - 2) * 180;
+}
