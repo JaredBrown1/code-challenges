3140bbe471d484ffa33c70a35df3c4d9b55d8fed
Jared Brown <jaredbrown1@hotmail.com>
2021-11-16 20:54:12 -0800

    edabit challenge

--- edabit/edabit.js
+++ edabit/edabit.js
@@ -301,3 +301,10 @@ function isSeven(x) {
 	}
 	return false;
 }
+
+// Given an object containing counts of both upvotes and downvotes,
+// return what vote count should be displayed. This is calculated by subtracting the number of downvotes from upvotes.
+
+function getVoteCount(votes) {
+	return votes.upvotes - votes.downvotes;
+}
