93485239a8b0fb605ada95a296bc6659f633dbbe
Jared Brown <jaredbrown1@hotmail.com>
2021-11-16 21:00:12 -0800

    edabit challenge

--- edabit/edabit.js
+++ edabit/edabit.js
@@ -308,3 +308,9 @@ function isSeven(x) {
 function getVoteCount(votes) {
 	return votes.upvotes - votes.downvotes;
 }
+
+// Create a function that takes an equation (e.g. "1+1"), and returns the answer.
+
+function equation(s) {
+	return eval(s);
+}
