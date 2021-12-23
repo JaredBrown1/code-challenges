6354437b2878d7526cc940503e1db949387120bc
Jared Brown <jaredbrown1@hotmail.com>
2021-12-22 17:37:49 -0800

    edabit challenge

--- edabit/edabit.js
+++ edabit/edabit.js
@@ -335,3 +335,21 @@ function area(h, w) {
 function eq(evaluate) {
 	return eval(evaluate);
 }
+
+// Write a function that checks whether a person can watch an MA15+ rated movie.
+// One of the following two conditions is required for admittance:
+
+// - The person is at least 15 years old.
+// - They have parental supervision.
+
+function acceptIntoMovie(age, isSupervised) {
+	if (
+		(age >= 15 && isSupervised === false) ||
+		(age >= 15 && isSupervised === true)
+	) {
+		return true;
+	} else if (age < 15 && isSupervised === true) {
+		return true;
+	}
+	return false;
+}
