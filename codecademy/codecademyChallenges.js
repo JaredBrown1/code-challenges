c2396fb8b932d837099d5c58fcc8b23361bf795d
Jared Brown <jaredbrown1@hotmail.com>
2021-12-26 12:03:03 -0800

    codecademy challenge

--- codecademy/codecademyChallenges.js
+++ codecademy/codecademyChallenges.js
@@ -123,3 +123,13 @@ const calculateWeight = (earthWeight, planet) => {
 const truthyOrFalsy = (value) => {
 	return value ? true : false;
 };
+
+// A person’s number of imaginary friends are always 25% (or 1/4) of their total friends.
+
+// Write a function, numImaginaryFriends(), that takes in the total number of friends a person has and returns the number of imaginary friends they have.
+
+// Since friends can only come in whole numbers, be sure to round your result up to the nearest whole number before returning it.
+
+const numImaginaryFriends = (totalFriends) => {
+	return Math.ceil(totalFriends / 4);
+};
