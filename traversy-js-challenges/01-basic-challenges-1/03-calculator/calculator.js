aad4d7a548527984429508b300d43dcd36a39779
Jared Brown <jaredbrown1@hotmail.com>
2023-09-21 21:28:18 -0700

    calculator

--- traversy-js-challenges/01-basic-challenges-1/03-calculator/calculator.js
+++ traversy-js-challenges/01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,13 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+	return operator === '+'
+		? num1 + num2
+		: operator === '-'
+		? num1 - num2
+		: operator === '*'
+		? num1 * num2
+		: operator === '/'
+		? num1 / num2
+		: null;
+}
 
 module.exports = calculator;
