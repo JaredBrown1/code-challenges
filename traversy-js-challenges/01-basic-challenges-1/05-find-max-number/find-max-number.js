43d41822ee08734186374aed77e443e1f5da91f1
Jared Brown <jaredbrown1@hotmail.com>
2023-09-23 13:48:32 -0700

    find max number

--- traversy-js-challenges/01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ traversy-js-challenges/01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,9 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+	let num = 0;
+	for (let i = 0; i < arr.length; i++) {
+		num < arr[i] ? (num = arr[i]) : null;
+	}
+	return num;
+}
 
 module.exports = findMaxNumber;
