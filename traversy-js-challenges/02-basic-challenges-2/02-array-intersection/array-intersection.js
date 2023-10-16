17d63fcf1a448e5a0e145dd4c1409993cb8b6efb
Jared Brown <jaredbrown1@hotmail.com>
2023-10-15 21:16:33 -0700

    array intersection

--- traversy-js-challenges/02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ traversy-js-challenges/02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,15 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+	let newArr = [];
+
+	for (let i = 0; i < arr1.length; i++) {
+		let num = arr1[i];
+
+		if (arr2.includes(num)) {
+			newArr.push(num);
+		}
+	}
+
+	return newArr;
+}
 
 module.exports = arrayIntersection;
