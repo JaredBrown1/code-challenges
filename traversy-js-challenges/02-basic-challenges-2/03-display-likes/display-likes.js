9d3ea90f78171af39d20ae4efe35a3e591616bc2
Jared Brown <jaredbrown1@hotmail.com>
2023-10-15 21:43:00 -0700

    display likes

--- traversy-js-challenges/02-basic-challenges-2/03-display-likes/display-likes.js
+++ traversy-js-challenges/02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,17 @@
-function displayLikes() {}
+function displayLikes(likes) {
+	if (likes.length === 0) {
+		return "no one likes this";
+	} else if (likes.length === 1) {
+		return `${likes[0]} likes this`;
+	} else if (likes.length === 2) {
+		return `${likes[0]} and ${likes[1]} like this`;
+	} else if (likes.length === 3) {
+		return `${likes[0]}, ${likes[1]} and ${likes[2]} like this`;
+	} else if (likes.length === 4) {
+		return `${likes[0]}, ${likes[1]} and ${likes.length - 2} others like this`;
+	} else if (likes.length === 5) {
+		return `${likes[0]}, ${likes[1]} and ${likes.length - 2} others like this`;
+	}
+}
 
 module.exports = displayLikes;
